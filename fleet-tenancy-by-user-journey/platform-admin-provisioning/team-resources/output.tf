2125a584a80f1d68d1dd44fcecde33d458d8132c
Steven Linde <stevenlinde@google.com>
2024-01-16 05:18:27 +0000

    overdue terraform fmt

--- fleet-tenancy-by-user-journey/platform-admin-provisioning/team-resources/output.tf
+++ fleet-tenancy-by-user-journey/platform-admin-provisioning/team-resources/output.tf
@@ -4,7 +4,7 @@
 output "connect_gateway_hosts" {
   // per https://github.com/binxio/gcp-private-gke-connect-gateway-deployment-example
   value = [
-    for binding in google_gke_hub_membership_binding.acme_scope_clusters :  "https://connectgateway.googleapis.com/v1/projects/${local.fleet_project_id}/locations/${binding.location}/gkeMemberships/${binding.membership_id}"
+    for binding in google_gke_hub_membership_binding.acme_scope_clusters : "https://connectgateway.googleapis.com/v1/projects/${local.fleet_project_id}/locations/${binding.location}/gkeMemberships/${binding.membership_id}"
   ]
 }
 
