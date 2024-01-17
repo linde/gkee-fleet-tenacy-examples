99c84001e28963da631b73925843bca1c5e6db37
Steven Linde <stevenlinde@google.com>
2024-01-17 01:40:14 +0000

    clarifying the connect usage

--- fleet-tenancy-by-user-journey/platform-admin-provisioning/team-self-service-namespace/providers.tf
+++ fleet-tenancy-by-user-journey/platform-admin-provisioning/team-self-service-namespace/providers.tf
@@ -1,4 +1,8 @@
 
+// here we use the output variable from the team-resources project. they come in an array,
+// one for each cluster bound to the team scope. each provider can only deal with one 
+// connection so you need a specific provider for each cluster, respectively. this provider
+// gets the first one, ie the one with subscript 0.
 
 provider "kubernetes" {
 
